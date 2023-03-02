b138c18c06dd0332e9a7ce740f30771f643df5ac
amboob223 <monk8896@gmail.com>
2023-03-02 05:06:08 -0500

    heyy

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: 'postgresql://gsite_user:fEUGYADX1ubCrYP5dB2bXtBlyTDiTioD@dpg-cg05gat269vdqrfpj2g0-a:5432/gsite'
+    connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
